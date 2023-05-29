b086f0be54c0afad7a91dc26683ca31a8ed0f7ef
Marcos Garcia herranz <marcogar@c1r16s4.42madrid.com>
2023-05-29 17:20:44 +0200

    Update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: marcogar <marcogar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 09:41:46 by marcogar          #+#    #+#             */
-/*   Updated: 2023/05/29 16:03:12 by marcogar         ###   ########.fr       */
+/*   Updated: 2023/05/29 17:20:22 by marcogar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,32 @@ typedef struct {
     int y;
 } Character;
 
-void key_press_hook(int key, void *param)
+void move_character(Character *character, int keycode)
 {
-	
+    // Actualizar la posición del personaje según la tecla presionada
+    switch (keycode) {
+        case 119:  // W
+            character->y -= 1;  // Mover hacia arriba
+            break;
+        case 115:  // S
+            character->y += 1;  // Mover hacia abajo
+            break;
+        case 97:   // A
+            character->x -= 1;  // Mover hacia la izquierda
+            break;
+        case 100:  // D
+            character->x += 1;  // Mover hacia la derecha
+            break;
+        default:
+            break;
+    }
+}
+
+int key_press_hook(int keycode, void *param)
+{
+    Character *character = (Character *)param;
+    move_character(character, keycode);
+    return 0;
 }
 int	main(void)
 {
@@ -38,7 +61,10 @@ int	main(void)
 	int		height;
     int     i;
     int     j;
-
+	Character character;
+	
+	character.x = 0;
+    character.y = 0;
     i = 0;
     j = 0;
 	mlx = mlx_init();
@@ -62,7 +88,7 @@ int	main(void)
         j++;
 	}
 	imagen.img = mlx_xpm_file_to_image(mlx, "xpm/dino.xpm", &width, &height);
-	mlx_key_hook(mlx_win, key_press_hook, NULL);
-	mlx_put_image_to_window(mlx, mlx_win, imagen.img, 64, 64);
+	mlx_key_hook(mlx_win, key_press_hook, (void *)&character);
+	mlx_put_image_to_window(mlx, mlx_win, imagen.img, (character.x * 64), (character.y * 64));
 	mlx_loop(mlx);
 }
