e3e1f5eeb702056d7d478c54b746e9c85767903e
marcosgh <marcosgh@linuxmanjaro.com>
2023-05-29 12:30:40 +0200

    Update

--- main.c
+++ main.c
@@ -10,7 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/lib.h"
+#include "mlx/mlx.h"
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
 int	main(void)
 {
